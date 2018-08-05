c09e85c0e83a1c54b5a416a50c533b902359f4ca
Thomas Van Winckel <vanwinckelthomas@gmail.com>
2018-08-05 11:43:43 +0200

    Setting a default context path for our web application.

---
+++ src/main/java/com/axxes/helloWorld/Config.java
@@ -0,0 +1,22 @@
+package com.axxes.helloWorld;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    /**
+     * This Bean allows us to set a default context path for our web application.
+     *
+     * Tip: you can set the property 'server.servlet.path=/axxes' to get the same result
+     */
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+    webServerFactoryCustomizer() {
+        return factory -> factory.setContextPath("/axxes");
+    }
+}
